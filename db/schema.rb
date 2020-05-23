f6a568bc94e814d78b9b5ed207706c3d88cec32e
SahSantoshh <santosh101410@gmail.com>
2020-05-23 17:02:02 +0545

    account and contact relationship added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_074107) do
+ActiveRecord::Schema.define(version: 2020_05_23_111053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_05_23_074107) do
     t.string "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "account_id"
+    t.index ["account_id"], name: "index_contacts_on_account_id"
   end
 
   create_table "push_topics", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_05_23_074107) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "contacts", "accounts"
 end
